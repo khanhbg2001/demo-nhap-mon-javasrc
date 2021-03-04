a9f36a206379453af76a68874387bd0a511ff9b4
khanhbg2001 <khanhbg2001tt@gmil.com>
2021-03-04 18:59:49 +0700

    comit

---
+++ input.js
@@ -0,0 +1,6 @@
+function tinhtong(){
+    let st1 = document.getElementById("st1").value
+    let st2 = document.getElementById("st2").value
+    let tong = st1 + st2
+    document.getElementById("ketqua").innerText = tong
+}
